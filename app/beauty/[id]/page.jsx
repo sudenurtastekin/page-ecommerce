275689e69dfee5727689b3345134c0a605479ce1
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-09-28 17:04:43 +0300

    index parametresi eklendi

--- app/beauty/[id]/page.jsx
+++ app/beauty/[id]/page.jsx
@@ -18,18 +18,20 @@ export default async function ProductsDetail({ params }) {
           <h2>{data.title}</h2>
           <img src={data.thumbnail} alt={data.title} />
         </div>
+
         <div className="Comments">
           <h5>{data.description}</h5>
-          {data.reviews.map(x => (
+          {data.reviews?.map((x, index) => ( 
             <li key={index} className="comments-list">
-              <p><strong>{x.reviewerName}:</strong>{x.comment}</p>
+              <p><strong>{x.reviewerName}:</strong> {x.comment}</p>
               <p>{x.date}</p>
             </li>
           ))}
+
           <hr />
           <span className="BudgetBtns">
             <button>➖</button>
-              <span>0</span>
+            <span>0</span>
             <button>➕</button>
           </span>
           <button className='AddBtn'>Add to cart</button>
